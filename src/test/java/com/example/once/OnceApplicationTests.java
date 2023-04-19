d14cedc446be47fae8a8be4497215915cd5dc78a
nqs <2952228216@qq.com>
2023-04-19 23:07:22 +0800

    123

---
+++ src/test/java/com/example/once/OnceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.once;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
